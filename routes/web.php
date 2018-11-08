ce9f244a4f4c7a521640dba8c096e4f81df31029
3a532043 <s3a532043@student.ncut.edu.tw>
2018-11-08 20:03:43 +0800

    為Route命名

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::get('welcome', function () {
     return view('welcome');
 });
-Route::get('hello/{name}', function ($name) {
+Route::get('hello/{name?}', ['as'=>'hello.index', function ($name='Everybody') {
     return 'Hello, '.$name;
-});
-});
+
+}]);
