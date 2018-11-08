c687b1bcbeabe69f13d8faba64907f24df2b3d4a
3a532043 <s3a532043@student.ncut.edu.tw>
2018-11-08 20:10:04 +0800

    設定Route前置 

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::get('say/{name?}', ['as'=>'hello.index', function ($name='Everybody') {
     return 'Hello, '.$name;
 
 }]);
+Route::group(['prefix'=>'admin'], function () {
+    Route::get('dashboard', function () {
+        return 'dashboard';
+    });
+
+});
+
